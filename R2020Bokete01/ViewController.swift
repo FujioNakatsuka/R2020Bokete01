99cb4819fec8d17790a71f3ef0ce69a8a44a9fc4
Fujio Nakatsuka <com.nifty.cbm01799>
2020-06-11 22:59:20 +0900

    もうすぐ23；00！何とか日付変更線を越える前に。。。。

--- R2020Bokete01/ViewController.swift
+++ R2020Bokete01/ViewController.swift
@@ -7,14 +7,105 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import SDWebImage
+import Photos
+
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var odaiImageView: UIImageView!
+    
+    @IBOutlet weak var commentTextView: UITextView!
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    var count = 0
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    super.viewDidLoad()
+       
+        commentTextView.layer.cornerRadius = 20.0
+        
+        PHPhotoLibrary.requestAuthorization{(status) in switch(status){
+            
+        case.authorized: break
+        case.denied: break
+        case.notDetermined: break
+        case.restricted: break
+            
+            
+            }
+            
+        }
+   
+        getImages(keyword: "funny")
+        
     }
-
-
+    func getImages(keyword:String){
+        
+        let url = "https://pixabay.com/api/?key=2963093-768f9ffc11d874c5a568a82ee&q=\(keyword)"
+        
+        AF.request(url,method: .get,parameters: nil ,encoding: JSONEncoding.default).responseJSON{
+            (response) in
+            
+            switch response.result{
+                
+            case.success:
+                let json:JSON = JSON(response.data as Any)
+            
+                let imageString = json["hits"][self.count]["webformatURL"].string
+                self.odaiImageView.sd_setImage(with: URL(string: imageString!), completed: nil)
+                
+            case.failure(let error):
+                
+                print(error)
+                
+            }
+            
+        }
+    }
+    
+    @IBAction func nextOdai(_ sender: Any) {
+        count = count + 1
+ 
+        if searchTextField.text == ""{
+            
+            getImages(keyword: "funny")
+            
+        }else{
+            
+            getImages(keyword: searchTextField.text!)
+            
+        }
+        
+    }
+    
+    @IBAction func searchAction(_ sender: Any) {
+        
+        self.count = 0
+       
+        if searchTextField.text == ""{
+            
+            getImages(keyword: "funny")
+            
+        }else{
+            
+            getImages(keyword: searchTextField.text!)
+            
+        }
+    }
+    
+    @IBAction func next(_ sender: Any) {
+        
+        performSegue(withIdentifier: "next", sender: nil)
+        
+    }
+    
+    
+    
+    
+    
 }
 
