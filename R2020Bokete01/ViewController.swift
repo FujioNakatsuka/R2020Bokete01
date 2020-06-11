f9fd35d5e8f12895302e0c5b8e5731d6b81a5c4f
Fujio Nakatsuka <com.nifty.cbm01799>
2020-06-11 16:44:43 +0900

    Initial Commit

---
+++ R2020Bokete01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  R2020Bokete01
+//
+//  Created by 中塚富士雄 on 2020/06/11.
+//  Copyright © 2020 中塚富士雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
